d3aa4ee94a86dc9201e2c4498f57d39a202d2873
Surya Sharma <suryasharma73077@gmail.com>
2026-04-02 23:24:17 +0530

    Pages

--- Debate-Arena-AI/src/App.jsx
+++ Debate-Arena-AI/src/App.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
-
+import Home from './pages/home'
+import About from './pages/about'
+import Contact from './pages/contact'
 const App = () => {
   return (
-    <div>App</div>
+    <div className='font-bold text-4xl p-4 m-9'>
+      <Home/>
+      <About/>
+      <Contact/>
+    </div>
   )
 }
 
